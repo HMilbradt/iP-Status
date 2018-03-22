1690204eaac61db52668a05cb240a4f48e7dfc0d
HMilbradt <harrison.milbradt@gmail.com>
2018-03-21 22:26:56 -0700

    Initial Commit

---
+++ iP Status/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  iP Status
+//
+//  Created by Harrison Milbradt on 2018-03-21.
+//  Copyright © 2018 Harrison Milbradt. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ iP Status/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iP Status
+//
+//  Created by Harrison Milbradt on 2018-03-21.
+//  Copyright © 2018 Harrison Milbradt. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
